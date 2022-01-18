2d5cb1b3a12ac5e46c6a2f15f2513ea4c801f244
Mihai Dornescu <dornescu@gmail.com>
2022-01-18 17:45:52 +0200

    search box

--- assets/icons/ChevronRight.js
+++ assets/icons/ChevronRight.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const ChevronRight = () => {
 	return (
-		<div>
+		<div className='bg-pink-300 rounded-md text-white mr-2'>
 			<svg
 				 role="img" className='w-6 h-6' preserveAspectRatio="xMidYMid meet" viewBox="0 0 24 24">
 				<g fill="none">
--- components/FoodTemplate/SearchBox.js
+++ components/FoodTemplate/SearchBox.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ChevronRight from "../../assets/icons/ChevronRight";
-import search from '../../assets/icons/search.svg'
+import { MdSearch, MdOutlineKeyboardArrowDown } from "react-icons/md";
+
 
 const SearchBox = () => {
 	return (
@@ -10,11 +11,17 @@ const SearchBox = () => {
 				<h1 className='text-2xl w-36'>Popular Menu</h1>
 			</div>
 			<div className='pt-8'>
-				<div>
-					<input type="text" placeholder='Search'/>
-					<button>
-						<img src={search} alt="search"/>
+				<div className='bg-slate-50 flex py-4 rounded-md px-4 md:items-center  md:w-96 md:mx-auto'>
+					<input type="text" placeholder='Search' className='bg-slate-50 w-64 md:w-full'/>
+					<button className='md:pr-4'>
+						<MdSearch />
 					</button>
+					<div className='flex flex-1 items-center justify-end'>
+						<div className='bg-pink-300 text-white p-2 rounded-md'>
+							<MdOutlineKeyboardArrowDown />
+						</div>
+
+					</div>
 				</div>
 
 			</div>
