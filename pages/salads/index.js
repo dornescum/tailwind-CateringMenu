19fbd237c04837b5f3d853e342471f9e9add74a6
Mihai Dornescu <dornescu@gmail.com>
2022-01-20 16:55:05 +0200

    products salads

--- pages/salads/index.js
+++ pages/salads/index.js
@@ -1,11 +1,38 @@
 import React from 'react';
+import path from "path";
+import fs from "fs/promises";
 
-const Index = () => {
+const Index = ({salads}) => {
+	console.log(salads);
 	return (
 		<div>
-			Salads
+			{salads.map((salad)=>{
+				const {id, title, description, price, img}=salad;
+				return <div key={salad.id}>
+					<p>{title}</p>
+					<p>{description}</p>
+					<p>{price} Euro</p>
+				</div>
+			})}
 		</div>
 	);
 };
 
+
+export async function getStaticProps() {
+	const filePath = path.join(process.cwd(), 'data', 'products.json');
+	// console.log(filePath);
+	const jsonData = await fs.readFile(filePath);
+	// console.log(jsonData);
+	const data = JSON.parse(jsonData);
+	// console.log(data);
+
+	return {
+		props: {
+			// products:[{id: 'p1', title: 'product 1'}]
+			salads: data.salads
+		},
+		revalidate: 10
+	};
+}
 export default Index;
