2119fdfab4837815c3e836b2962387586d62b439
Mihai Dornescu <dornescu@gmail.com>
2022-01-22 08:20:28 +0200

    thinkpad pasta

--- pages/pasta/index.js
+++ pages/pasta/index.js
@@ -9,3 +9,43 @@ const Index = () => {
 };
 
 export default Index;
+ export const getStaticProps = async (context) => {
+	const id = context.params.id;
+	console.log(context);
+	// const res = await fetch(`${server}/api/products/${id}`);
+	const products = await res.json();
+	return {
+		props: {
+			products,
+		},
+	};
+};
+
+export const getStaticPaths = async () => {
+	const res = await fetch(`${server}/data/products.json`);
+	const products = await res.json();
+	console.log(typeof products);
+	const ids = products.data.map((product) => product.id);
+	const paths = ids.map((id) => ({params: {id: id.toString()}}));
+	return {
+		paths,
+		fallback: false,
+	};
+};
+
+// 2 este buna
+export async function getStaticProps() {
+	const filePath = path.join(process.cwd(), 'data', 'products.json');
+	// console.log(filePath);
+	const jsonData = await fs.readFile(filePath);
+	// console.log(jsonData);
+	const data = JSON.parse(jsonData);
+	// console.log(data);
+	return {
+		props: {
+			// products:[{id: 'p1', title: 'product 1'}]
+			salads: data.salads
+		},
+		revalidate: 10
+	};
+}
