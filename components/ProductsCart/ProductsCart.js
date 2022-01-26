546c02be314024a3989ecca0a486ffd35ce5a6d4
Mihai Dornescu <dornescu@gmail.com>
2022-01-26 11:44:32 +0200

    cart & login link

--- components/ProductsCart/ProductsCart.js
+++ components/ProductsCart/ProductsCart.js
@@ -1,13 +1,31 @@
 import React, {useContext} from 'react';
 import {ValueContext} from "../../context/ValueContext";
+import Link from "next/link";
 
 const ProductsCart = () => {
 	const {initialValue, setInitialValue} = useContext(ValueContext);
 	const value = ((initialValue * Math.PI / Math.random()) / Math.random()).toFixed(2);
 
 	return (
-		<div className='absolute bottom-10 -left-10 rounded-md  bg-primary text-white h-24 w-48 flex items-center justify-center opacity-100'>
-			<p>{initialValue} items = {value} &euro;</p>
+		<div className='absolute bottom-10 -left-10 rounded-md  bg-primary text-white h-48 w-48
+		flex items-center justify-center opacity-100' data-testid='product-cart'>
+			<div className='flex flex-col' data-testid='cart'>
+				<p data-testid=''>{initialValue} items = {value} &euro;</p>
+				<br/>
+				<hr/>
+				<div className='flex justify-between' data-testid='cart-value'>
+					<p>Total </p>
+					<p data-testid='value-cart'>  {value} &euro;</p>
+				</div>
+				<div className='bg-white text-primary my-4 rounded-md' data-testid='link-login'>
+					<Link href="/login">
+						<a>
+							<p className='py-2 px-2'> Place my order </p>
+						</a>
+					</Link>
+				</div>
+			</div>
+
 		</div>
 	);
 };
